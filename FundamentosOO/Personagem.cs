1e11e5023f924ed258e154f9aaab602e90c70513
ÍtaloSenai2A <italooosenai@gmail.com>
2024-08-20 16:20:49 -0300

    Incluindo as classes dos Personagens do jogo.

--- FundamentosOO/Personagem.cs
+++ FundamentosOO/Personagem.cs
@@ -36,28 +36,47 @@ namespace FundamentosOO
         }
         public virtual void batalhar(Personagem p)
         {
-            int valorAtaque = this.atacar();
-            int valorDefesaInimigo = p.defender();
-            int danoDeVida = valorAtaque - valorDefesaInimigo;
-            if (danoDeVida <= 0)
+            bool vencedor = false;
+            while (vencedor == false)
             {
-                danoDeVida = 1;
-            }
-            p.Vida = p.Vida - danoDeVida;
-            Console.WriteLine("Dano na Vida do Inimigo em " + danoDeVida);
-            Console.WriteLine("O nível de vida do Inimigo agora é " + p.Vida);
+                int valorAtaque = this.atacar();
+                int valorDefesaInimigo = p.defender();
+                int danoDeVida = valorAtaque - valorDefesaInimigo;
+                if (danoDeVida <= 0)
+                {
+                    danoDeVida = 1;
+                }
+                p.Vida = p.Vida - danoDeVida;
+                Console.WriteLine("Dano na Vida do Inimigo em " + danoDeVida);
+                Console.WriteLine("O nível de vida do Inimigo agora é " + p.Vida);
 
-            int valorAtaqueInimigo = p.atacar();
-            int valorDefesa = this.defender();
-            int danoDeVida2 = valorAtaqueInimigo - valorDefesa;
-            if (danoDeVida2 <= 0)
-            {
-                danoDeVida2 = 1;
+                int valorAtaqueInimigo = p.atacar();
+                int valorDefesa = this.defender();
+                int danoDeVida2 = valorAtaqueInimigo - valorDefesa;
+                if (danoDeVida2 <= 0)
+                {
+                    danoDeVida2 = 1;
+                }
+                this.Vida = this.Vida - danoDeVida2;
+                Console.WriteLine("Dano na Vida do Atacante em " + danoDeVida2);
+                Console.WriteLine("O nível de vida do Atacante agora é " + this.Vida);
+
+                Console.WriteLine("====================================================");
+
+                if (this.Vida <= 0)
+                {
+                    Console.WriteLine("O personagem " + p.Nome + "Venceu!");
+                    vencedor = true;
+                }
+                if (p.Vida <= 0)
+                {
+                    Console.WriteLine("O Personagem " + this.Nome + " Venceu!");
+                    vencedor = true;
+                }
             }
-            this.Vida = this.Vida - danoDeVida2;
-            Console.WriteLine("Dano na Vida do Atacante em " + danoDeVida2);
-            Console.WriteLine("O nível de vida do Atacante agora é " + this.Vida);
         }
+
+
 
     }
 }
