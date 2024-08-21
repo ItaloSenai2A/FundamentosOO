a9f7fde1400f760b9199e92b81147081ca78ca87
ÍtaloSenai2A <italooosenai@gmail.com>
2024-08-21 08:17:48 -0300

    Finalizado a atividade do Game

--- FundamentosOO/Personagem.cs
+++ FundamentosOO/Personagem.cs
@@ -20,7 +20,14 @@ namespace FundamentosOO
         // Metodos
 
         public virtual void apresentarSe() { }
-        public virtual void evoluir() { }
+        public virtual void evoluir()
+        {
+            this.Nivel += 2;
+            this.Forca *= 2;
+            this.Inteligencia *= 2;
+            this.Agilidade *= 2;
+            Console.WriteLine("O jogador " + this.Nome + " evoluiu para o nível " + this.Nivel + ". Seus novos valores são: \nAgilidade" + this.Agilidade + "\nInteligência: " + this.Inteligencia + "\nForça: " + this.Forca);
+        }
         public virtual int atacar()
         {
             int pontosAtaque = 0;
@@ -67,11 +74,13 @@ namespace FundamentosOO
                 {
                     Console.WriteLine("O personagem " + p.Nome + "Venceu!");
                     vencedor = true;
+                    p.evoluir();
                 }
                 if (p.Vida <= 0)
                 {
                     Console.WriteLine("O Personagem " + this.Nome + " Venceu!");
                     vencedor = true;
+                    this.evoluir();
                 }
             }
         }
